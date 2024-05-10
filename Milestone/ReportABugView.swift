95f4b46d5982938ab52e8af1c9391b96cb4bfbc4
Nikolomoec <nikolomoec@gmail.com>
2024-05-10 11:05:33 +0300

    - Add Settings View to ContentView - Add Report a Bug Sheet - Add Share App Sheet - Add Twitter Account Redirect - Add Rate this app button

---
+++ Milestone/ReportABugView.swift
@@ -0,0 +1,18 @@
+//
+//  ReportABugView.swift
+//  Milestone
+//
+//  Created by Nikita Kolomoec on 09.05.2024.
+//
+
+import SwiftUI
+
+struct ReportABugView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportABugView()
+}
---
+++ Milestone/ShareAppView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareAppView.swift
+//  Milestone
+//
+//  Created by Nikita Kolomoec on 09.05.2024.
+//
+
+import SwiftUI
+
+struct ShareAppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareAppView()
+}
