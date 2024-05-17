5fe93217284fec02f0994822d14bd235394c0b6b
Nikolomoec <nikolomoec@gmail.com>
2024-05-17 09:31:26 +0300

    - Add some more long press button details

--- Milestone/GoalButtonView.swift
+++ Milestone/GoalButtonView.swift
@@ -18,9 +18,28 @@ struct GoalButtonView: View {
     @State private var linesArray = [DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine(), DashLine()]
     @State private var nextCreationTime = Date.now
     
+    let randomImpactRectangleColor = [
+        Color(red: 0.10, green: 0.36, blue: 0.47),
+        Color(red: 0.67, green: 0.75, blue: 0.76),
+        Color(red: 0.52, green: 0.63, blue: 0.67),
+        Color(red: 0.11, green: 0.31, blue: 0.36),
+        Color(red: 0.49, green: 0.58, blue: 0.60)
+    ].shuffled()
+    
     var body: some View {
         ZStack {
-            
+            // 3 random "Impact Rectangles" when button long press is completed
+            ForEach(0..<5) { num in
+                ImpactRectangle()
+                    .frame(width: Double.random(in: 10...60), height: completedLongPress ? 300 : 0)
+                    .padding(.top, 200)
+                    .foregroundStyle(randomImpactRectangleColor[num])
+                    .rotationEffect(.degrees(Double(70 * num)))
+                    .animation(.default, value: completedLongPress)
+                    .opacity(completedLongPress ? 0 : Double.random(in: 0.7...1))
+                    .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/.delay(0.1), value: completedLongPress)
+                    .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+            }
             
             // Start Button
             Circle()
@@ -42,7 +61,7 @@ struct GoalButtonView: View {
                 }
             
             // Flying Lines
-            ForEach(Array(linesArray.enumerated()), id: \.element) { index, line in
+            ForEach(linesArray) { line in
                 Rectangle()
                     .foregroundStyle(.white)
                     .rotationEffect(line.angle + .degrees(90))
@@ -51,6 +70,11 @@ struct GoalButtonView: View {
                     .animation(.easeIn(duration: line.duration).speed(isPressing ? 1 : 100), value: isPressing)
                     .opacity(isPressing ? howLongButtonPressed / 7 : 0)
             }
+            
+            Image(systemName: "checkmark")
+                .font(.system(size: 90, weight: .black))
+                .opacity(completedLongPress ? 1 : 0)
+                .animation(.default.delay(0.1).speed(100), value: completedLongPress)
             
             Circle()
                 .stroke(lineWidth: completedLongPress ? 0 : 100)
@@ -102,5 +126,23 @@ struct GoalButtonView: View {
         AnimatedGradBackground(type: .blackWhite)
         
         GoalButtonView()
+    }
+}
+
+// When completedLongPress = true, it will create some of them to show the "blowing up button" effect
+struct ImpactRectangle: Shape {
+    
+    let randomDevisor = Double.random(in: 7...15)
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX + rect.width / randomDevisor, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX - rect.width / randomDevisor, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        
+        return path
     }
 }
